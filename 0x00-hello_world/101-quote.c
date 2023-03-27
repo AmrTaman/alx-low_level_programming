524d541df68d4ae56be2da3ebac40c1a6347af9c
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-03-27 04:26:46 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /*
- * main - testing the error streaming message
- *
- * Return: one
- */
+  * main - testing the error streaming message
+  *
+  * Return: one
+  */
 int main(void)
 {
         char amr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
