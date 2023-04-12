f1dff8d1a815b2e4e36fa9223e6a553131e10fbb
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-04-13 01:40:44 +0200

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - alphabet
+ *
+ * Return: zero
+ */
+int main(void)
+{
+        int first;
+        int second;
+
+        for (first = '0'; first <= '9'; first++)
+        {
+                for (second = '0'; second <= '9'; second++)
+                {
+                        if (first != second && first < second)
+                        {
+                                putchar(first);
+                                putchar(second);
+                                if (first == '8' && second == '9')
+                                        break;
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
+}
