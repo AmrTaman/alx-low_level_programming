655eef399d591d360b360cd540184ad12de0d849
Amr <eldoski@gmail.com>
2023-04-12 17:03:29 -0700

    iam in

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,22 +6,31 @@
  */
 int main(void)
 {
-        int first;
-        int second;
+	int first;
+	int second;
+	int third;
 
-        for (first = '0'; first <= '9'; first++)
-        {
-                for (second = '0'; second <= '9'; second++)
-                {
-                        if (first != second && first < second)
-                        {
-                                putchar(first);
-                                putchar(second);
-                                if (first == '8' && second == '9')
-                                        break;
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
+	for (first = '0'; first <= '9'; first++)
+	{
+		for (second = '0'; second <= '9'; second++)
+		{
+			for (third = '0'; third <= '9'; third++)
+			{
+				if (first != second && second != third && first < third &&
+					second < third && first < second)
+				{
+					putchar(first);
+					putchar(second);
+					putchar(third);
+					if (first == '7' && second == '8' && third == '9')
+					{
+						putchar('\n');
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 }
