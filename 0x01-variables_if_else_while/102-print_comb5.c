2c847d4775d209c76422008b7ea620a1665ed011
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-04-13 06:58:42 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int calc(int a, int b, int c, int d)
 		putchar(b);
 		putchar(' ');
 		putchar(c);
-		putchar(d);   
+		putchar(d);
 		if (a == '9' && b == '8' && c == '9' && d == '9')
 		{
 			putchar('\n');
@@ -22,28 +22,28 @@ int calc(int a, int b, int c, int d)
 }
 int main(void)
 {
-	int a ;
-	int b ;
-	int c ;
-	int d ;
+	int a;
+	int b;
+	int c;
+	int d;
 
 	for (a = '0'; a <= '9' ; a++)
-	{                
+	{             
 		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
 				for (d = '0'; d <= '9'; d++)
 				{
-					if( a != c || b != d)
+					if (a != c || b != d)
 					{
 						if ( a == c && b < d )
 						{
-							calc(a,b,c,d);
+							calc(a, b, c, d);
 						}
 						else if (a < c )
 						{
-							calc(a,b,c,d);
+							calc(a, b, c, d);
 						}
 					}
 				}
