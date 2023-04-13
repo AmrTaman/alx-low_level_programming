ba0962ca11fadd0b0c503a68d921fe1f238883de
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-04-13 06:53:54 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,58 +4,51 @@
  *
  * Return: Zero
  */
-int calc( int a,int b,int c,int d){
-
-                putchar(a);
-                putchar(b);
-                putchar(' ');
-                putchar(c);
-                putchar(d);   
-                        if (a == '9' && b == '8' && c == '9' && d == '9')
+int calc(int a, int b, int c, int d)
+{
+		putchar(a);
+		putchar(b);
+		putchar(' ');
+		putchar(c);
+		putchar(d);   
+		if (a == '9' && b == '8' && c == '9' && d == '9')
                 {
-                        putchar('\n');
-                        return (0);
-                }
-                
-                putchar(',');
-                putchar(' ');
-                return (0);
-
+			putchar('\n');
+			return (0);
+		}
+		putchar(',');
+		putchar(' ');
+		return (0);
 }
 int main(void)
 {
-        int a ;
-        int b ;
-        int c ;
-        int d ;
-  
+	int a ;
+	int b ;
+	int c ;
+	int d ;
 
-
-            for (a = '0'; a <= '9' ; a++){
-                
-             for (b = '0'; b <= '9'; b++){
-                for (c = '0'; c <= '9'; c++){
-                
-                  for (d = '0'; d <= '9'; d++){
-             
-                if( a != c || b != d){
-                    if ( a == c && b < d ){ 
-                    calc(a,b,c,d);
-                    
-                    }
-
-
-                
-                else if (a < c ){
-                     calc(a,b,c,d);       
-
-
-                }
-                
-                
-                
-
-                
-                }}}}}
-        return (0);
+	for (a = '0'; a <= '9' ; a++)
+	{                
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					if( a != c || b != d)
+					{
+						if ( a == c && b < d )
+						{
+							calc(a,b,c,d);
+						}
+						else if (a < c )
+						{
+							calc(a,b,c,d);
+						}
+					}
+				}
+			}
+		}
+	}
+	return (0);
 }
