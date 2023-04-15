821fab7e325df69e121afca29cc584d9427c11ee
Amr <amr@gmail.com>
2023-04-15 20:38:07 +0000

    table is ready

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include"main.h"
+/**
+ * print_times_table - printing times table till 15
+ */
+void print_times_table(int n)
+{
+	if (n <= 15)
+	{
+		int m;
+		int x;
+		int times;
+
+		for (m = 0; m <= n; m++)
+		{
+			_putchar('0');
+			for (x = 1; x <= n; x++)
+			{
+				times = m * x;
+				_putchar(',');
+				_putchar(' ');
+				if (times / 100 >= 1)
+					_putchar('0' + times / 100);
+				else
+				{
+					_putchar(' ');
+				}
+				if (times / 10 >= 1 && times < 100)
+					_putchar('0' + times / 10);
+				else if (times / 10 % 10 >= 0)
+					_putchar('0' + times / 10 % 10);
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar('0' + times % 10);
+			}
+			_putchar('\n');
+		}
+	}
+}
