9fcb5b804bd69be59d4fcb10e57b7af5f5111927
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-04-15 22:48:13 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					if (times / 10 >= 1 && times < 100)
-						_putchar('0' + times / 10);	
+						_putchar('0' + times / 10);
 					else
 					{
 						_putchar(' ');
