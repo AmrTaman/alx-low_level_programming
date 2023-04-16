f157f71c943e7f5bf6df1a1c2d0fd4ec61cc32a5
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-04-16 02:31:56 +0200

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - prints first 50 fibonacci numbers
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	long sum = 0;
+	long x = 1;
+	int m;
+
+	for (m = 0; m < 49; m++)
+	{
+		sum += x;
+		x += sum;
+		printf("%lu, ", sum);
+		if (m == 24)
+			printf("%lu\n", x);
+		else
+		{
+		printf("%lu, ", x);
+		}
+	}
+	return (0);
+}
