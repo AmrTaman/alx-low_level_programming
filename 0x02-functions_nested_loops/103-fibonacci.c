0c2349314517bea8bdb13e8ee23eb50ab024d44f
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-04-16 02:13:01 +0200

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - prints first 50 fibonacci numbers
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	long sum = 0;
+	long x = 1;
+	long m = 0;
+
+	while (True)
+	{
+		sum += x;
+		x += sum;
+		if (sum >= 4000000 || x >= 4000000)
+			break;
+		else
+		{
+			if (sum % 2 == 0)
+			{
+				m += sum;
+				printf("%lu", m);
+			}
+			else if (x % 2 == 0)
+			{
+				m += x;
+				printf("%lu", m);
+			}
+		}
+	}
+	return (0);
+}
