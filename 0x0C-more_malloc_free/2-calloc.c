0461f74569e53091a40d4ac7afae5f0051f5738a
AmrTaman <amrtaman4325@gmail.com>
2023-05-02 21:05:05 +0000

    yes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,10 +9,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	unsigned int i;
 
 	ptr = malloc(nmemb * size);
 	if (ptr == 0)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 	return (ptr);
 }
