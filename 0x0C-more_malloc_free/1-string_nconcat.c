246b1522dffbbd79be1d28ba8318cbcceda34f1b
AmrTaman <amrtaman4325@gmail.com>
2023-05-02 19:45:42 +0000

    yes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	if (n > s)
 		n = s;
-	for (s = 0; s < f + n; s++)
+	for (s = 0; s < f + n - 1; s++)
 	{
 		if (s < f)
 			ptr[s] = s1[s];
