91759dbe6efb30b719200ccaf5d842d929a98fb5
AmrTaman <amrtaman4325@gmail.com>
2023-05-02 20:56:41 +0000

    yes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,37 +1,18 @@
-/*****************************************************************************/
-/*                                                                           */
-/*                                               _____  ______    ____  ___  */
-/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
-/*                                             /  /_\  \|    |     \     /   */
-/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
-/*                                            \____|__  /_______ \/___/\  \  */
-/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
-/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
-/*                                                                           */
-/*****************************************************************************/
-
-#include "main.h"
-#include <stdlib.h>
+#include<stdlib.h>
+#include"main.h"
 /**
- * *_calloc - call
- * @nmemb: n
- * @size: size
- * Return: 0
+ * _calloc - allocates memory
+ * @nmemb: number of blocks
+ * @size: size of each block
+ *
+ * Return: ptr
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *arr;
-	unsigned int i;
+	void *ptr;
 
-	if (nmemb == 0 || size == 0)
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
 		return (NULL);
-
-	arr = malloc(nmemb * size);
-	if (arr == NULL)
-		return (NULL);
-
-	for (i = 0; i < (nmemb * size); i++)
-		arr[i] = 0;
-
-	return (arr);
+	return (ptr);
 }
