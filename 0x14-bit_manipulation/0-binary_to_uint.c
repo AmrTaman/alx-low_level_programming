b8bd7a6c1083eaa86ddce3933d3f08edcc7ac313
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-05-05 05:38:26 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		number = 2 * number + (b[i] - '0');
 	}
