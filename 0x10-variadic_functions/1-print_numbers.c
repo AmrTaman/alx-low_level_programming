1734b4ef83ea2107a2478f63afafef1de13ab9a0
AmrTaman <amrtaman4325@gmail.com>
2023-05-08 22:24:30 +0000

    yes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,44 +1,21 @@
-/*****************************************************************************/
-/*                                                                           */
-/*                                               _____  ______    ____  ___  */
-/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
-/*                                             /  /_\  \|    |     \     /   */
-/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
-/*                                            \____|__  /_______ \/___/\  \  */
-/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
-/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
-/*                                                                           */
-/*****************************************************************************/
-
-#include<stdio.h>
 #include<stdarg.h>
 #include<stdio.h>
-
 /**
- * print_numbers - a function ...
- * @separator: the chaine
- * @n: the number
- * @...: the char
- *
- * Return: 1 or 0
+ * print_numbers - prints numbers
+ * @separator: seperator between numbers
+ * @n: number of values
  */
-void print_numbers(char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator,
+		const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index;
+	va_list num;
+	unsigned int x;
 
-	va_start(nums, n);
-
-	for (index = 0; index < n; index++)
-	{
-		printf("%d", va_arg(nums, int));
-
-		if (index != (n - 1) && separator != NULL)
-			printf("%s", separator);
-	}
-
+	if (separator == NULL || n == 0)
+		return;
+	va_start(num, n);
+	printf("%d", va_arg(num, int));
+	for (x = 0; x < (n - 1); x++)
+		printf("%s%d", separator, va_arg(num, int));
 	printf("\n");
-
-	va_end(nums);
 }
-
