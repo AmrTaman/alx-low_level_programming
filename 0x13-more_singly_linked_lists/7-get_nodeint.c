92492abecae157dce550800cb3ed450c6fcf5df5
AmrTaman <amrtaman4325@gmail.com>
2023-06-18 19:40:25 +0000

    yes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 
 	for (; x < index && head; x++)
-		head = head->index;
+		head = head->next;
 	if (index > x)
 		return (NULL);
 	return (head);
