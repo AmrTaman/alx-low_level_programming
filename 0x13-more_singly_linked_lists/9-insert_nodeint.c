7d4f3d844bab9ee34afa8b6426305d1187dd0b1e
AmrTaman <amrtaman4325@gmail.com>
2023-06-18 20:28:29 +0000

    yes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include<stdlib.h>
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
@@ -11,35 +11,19 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	unsigned int x = 0;
+	listint_t *current, *ptr = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	if (ptr == NULL)
 		return (NULL);
+	for (; x < idx - 1 && ptr; x++)
+		ptr = ptr->next;
+	if (x < idx - 1)
+		return (NULL);
+	current = malloc(sizeof(listint_t));
+	current->n = n;
+	current->next = ptr->next;
+	ptr->next = current;
+	return (current);
 
-	new->n = n;
-	new->next = NULL;
-
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
-
-	for (i = 0; temp && i < idx; i++)
-	{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
-	}
-
-	return (NULL);
 }
