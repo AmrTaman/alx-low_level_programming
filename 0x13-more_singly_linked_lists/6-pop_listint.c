63c5b781e3248321a71e92eff87978daf9639227
AmrTaman <amrtaman4325@gmail.com>
2023-06-18 17:40:36 +0000

    yes

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,37 +1,18 @@
-/*****************************************************************************/
-/*                                                                           */
-/*                                               _____  ______    ____  ___  */
-/* 6-pop_listint.c                              /  _  \ |    |    \   \/  /  */
-/*                                             /  /_\  \|    |     \     /   */
-/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
-/*                                            \____|__  /_______ \/___/\  \  */
-/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
-/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
-/*                                                                           */
-/*****************************************************************************/
-
-#include "lists.h"
-#include <stdlib.h>
-
-/**
- * pop_listint - a function ...
- * @head: the list
+#include"lists.h"
+/*
+ * pop_listint - deletes the head of a node
  *
- * Return: 1 or 0
+ * @head: pointer
+ * Return: *head
  */
-
 int pop_listint(listint_t **head)
 {
-	listint_t *tmp;
-	int n;
+	listint_t *current;
+	int x;
 
-	if (head == NULL || *head == NULL)
-		return (0);
-
-	tmp = (*head)->next;
-	n = (*head)->n;
-	free(*head);
-	*head = tmp;
-	return (n);
+	current = *head;
+	x = (*head)->n;
+	*head = (*head)->next;
+	free(current);
+	return(x);
 }
-
