1b7e11ebf0562b1cfb1ea991328b5e7540c76074
AmrTaman <amrtaman4325@gmail.com>
2023-06-19 20:58:08 +0000

    yes

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-int main(void)
+#include<stdlib.h>
+#include<unistd.h>
+int printf(const char *format, ...)
 {
-	printf("--> Please make me win!");
-	return (0);
+	write(1, "9 8 10 24 75 - 9\n", 17);
+	write(1, "congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
 }
