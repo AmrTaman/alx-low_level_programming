078c90927175a86ed539307cf13abfa8a10b2b77
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-06-20 00:45:41 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/AmrTaman/alx-low_level_programming/blob/09449688fd491b1493fe87148b5deda88849e84c/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=$(pwd)/libwin.so
+wget -P /tmp https://github.com/AmrTaman/alx-low_level_programming/blob/09449688fd491b1493fe87148b5deda88849e84c/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so
