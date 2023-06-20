b4e65367612f4d623e1f88823b836fe357ba6fc9
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-06-20 03:17:23 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/TamanAlwaysWin.so
+wget -P /tmp https://raw.github.com/AmrTaman/alx-low_level_programming/master/0x18-dynamic_libraries/TamanAlwaysWin.so
 export LD_PRELOAD=/tmp/TamanAlwaysWin.so
