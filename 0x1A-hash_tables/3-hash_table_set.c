78d953342d5bc5e2d0f656c80a769dac039cab4b
AmrTaman <amrtaman4325@gmail.com>
2023-07-07 13:06:09 +0000

    p

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -62,7 +62,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current_node, *node;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
