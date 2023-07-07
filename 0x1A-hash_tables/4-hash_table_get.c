d560873a38c41b1454a5dff93a7e11df5129ff95
AmrTaman <amrtaman4325@gmail.com>
2023-07-07 14:33:29 +0000

    u

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,7 @@
 #include"hash_tables.h"
 #include<stdlib.h>
 #include<string.h>
+#include<stdio.h>
 /**
  * hash_table_get - search
  * @ht: hash table
@@ -16,10 +17,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index]
-	while (node->next)
+	node = ht->array[index];
+	while (node)
 	{
-		if (strcmp(node->key, key))
+		if (strcmp(node->key, key) == 0)
 			return (node->value);
 		node = node->next;
 	}
