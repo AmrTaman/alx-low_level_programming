b4ccc62131c75d62393915ff29423425014cdf84
AmrTaman <amrtaman4325@gmail.com>
2023-07-07 14:40:05 +0000

    u

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,21 +11,22 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int long x = 0;
 	int m = 0;
 
-	if (ht == NULL)
-		return;
-	printf("{");
-	for (; x < ht->size; x++)
+	if (ht != NULL)
 	{
-		if (ht->array[x])
+		printf("{");
+		for (; x < ht->size; x++)
 		{
-			if (m == 0)
+			if (ht->array[x])
 			{
-				printf("'%s': '%s'", ht->array[x]->key, ht->array[x]->value);
-				m++;
-				continue;
+				if (m == 0)
+				{
+					printf("'%s': '%s'", ht->array[x]->key, ht->array[x]->value);
+					m++;
+					continue;
+				}
+				printf(", '%s': '%s'", ht->array[x]->key, ht->array[x]->value);
 			}
-			printf(", '%s': '%s'", ht->array[x]->key, ht->array[x]->value);
 		}
+		printf("}\n");
 	}
-	printf("}\n");
 }
