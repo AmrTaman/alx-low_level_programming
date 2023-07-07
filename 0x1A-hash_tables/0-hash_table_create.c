b74fd39790c63c34683d495d3abf170949bcc5bb
AmrTaman <amrtaman4325@gmail.com>
2023-07-07 09:22:43 +0000

    yes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,5 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
 	return (table);
 }
