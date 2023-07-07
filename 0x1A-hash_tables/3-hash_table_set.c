d2e79efadd076147410a8a69d965ecf098d6d748
AmrTaman <amrtaman4325@gmail.com>
2023-07-07 13:11:27 +0000

    p

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -72,7 +72,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = node;
 		return (1);
 	}
-	else if (strcmp(current_node->key, key) == 0)
+	else if (strcmp(current_node->key, key) == 0 && ht->array[index])
 	{
 		strcpy(current_node->value, value);
 		return (1);
