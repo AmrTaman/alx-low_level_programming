7e8229de9ae55a24e9d2ab2747e9d65c1d93e4eb
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-19 01:35:04 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int x = 0;
-	hash_node_t *node = NULL;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+	
 
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	node = calloc(table->size, sizeof(hash_node_t *));
-	if (node == NULL)
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
