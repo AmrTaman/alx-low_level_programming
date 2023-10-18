a1463ccb19f770b4fd225fd5f7be58d25188437e
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-19 01:04:44 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,9 +5,10 @@
  *
  * Return: pointer of table
  */
-hash_table_t *hash_table_create(unsigned long int size){
+hash_table_t *hash_table_create(unsigned long int size)
+{
 	unsigned long int x = 0;
-    hash_node_t* node;
+	hash_node_t* node;
 	hash_table_t* table = malloc (sizeof(hash_table_t));
 
     if (table == NULL)
