9fbf4e8cf54ef193b252a3e9c8ac4d4157de437b
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-19 18:47:52 +0300

    Create 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hashtable
+ * @ht: hash table pointer
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int x;
+	int count = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			if (count == 0)
+				printf("'%s': '%s'", ht->array[x]->key, ht->array[x]->value);
+			printf(", '%s': '%s'", ht->array[x]->key, ht->array[x]->value);
+			count++;
+		}
+	}
+	printf("}\n");
+}
