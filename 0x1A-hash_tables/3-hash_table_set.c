22862e34c9d8adb38c741e9abda0d9d1e6d678cf
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-19 03:46:34 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
     if (ht == NULL || key == NULL || !(strcmp(key, "")) || value == NULL)
         return (0);
-    hash = key_index(key, ht->size);
+    hash = key_index((const unsigned char *)key, ht->size);
     if (ht->array[hash] == NULL)
     {
         if (hash >= ht->size)
