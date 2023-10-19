839be6f4ab12e94e1933736c56b39a7a228aab02
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-19 20:07:30 +0300

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,21 +7,27 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int x;
 	int count = 0;
+	hash_node_t *node;
 
 	if (ht == NULL)
 		return;
 	printf("{");
 	for (x = 0; x < ht->size; x++)
 	{
-		if (ht->array[x] != NULL)
+		node = ht->array[x];
+		while(node)
 		{
-			if (count == 0)
-				printf("'%s': '%s'", ht->array[x]->key, ht->array[x]->value);
-			else
+			if (node != NULL)
 			{
-			printf(", '%s': '%s'", ht->array[x]->key, ht->array[x]->value);
+				if (count == 0)
+					printf("'%s': '%s'", node->key, node->value);
+				else
+				{
+					printf(", '%s': '%s'", ht->array[x]->key, ht->array[x]->value);
+				}
+				count++;
 			}
-			count++;
+			node = node->next
 		}
 	}
 	printf("}\n");
