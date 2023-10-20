8ec1ef738bf140c0abb9d7b95492331306c0038f
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-21 02:06:06 +0300

    Update 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -8,13 +8,13 @@
 shash_table_t *shash_table_create(unsigned long int size)
 {
 	unsigned long int x;
-
 	shash_table_t *ht = malloc(sizeof(shash_table_t));
+
 	ht->size = size;
 	ht->array = calloc(size, sizeof(shash_node_t *));
 	ht->shead = NULL;
 	ht->stail = NULL;
-	for (x= 0; x < ht->size; x++)
+	for (x = 0; x < ht->size; x++)
 		ht->array[x] = NULL;
 	return (ht);
 }
