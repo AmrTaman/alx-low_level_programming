ed259e1044a61d3d3e8ad93c0471cdb2c038750d
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-21 02:05:30 +0300

    Update 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,1 +1,20 @@
+#include"hash_tables.h"
+/**
+ * shash_table_create - creates a table
+ * @size: size of the table
+ *
+ * Return: pointer of the hastable
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	unsigned long int x;
 
+	shash_table_t *ht = malloc(sizeof(shash_table_t));
+	ht->size = size;
+	ht->array = calloc(size, sizeof(shash_node_t *));
+	ht->shead = NULL;
+	ht->stail = NULL;
+	for (x= 0; x < ht->size; x++)
+		ht->array[x] = NULL;
+	return (ht);
+}
