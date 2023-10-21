063d905deb42a64f4e650c8e40ce82531ee89d4d
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-21 04:48:44 +0300

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -76,7 +76,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-shash_node_t *create_node(shash_table_t *ht, const char *key, const char *value);
+shash_node_t *create_node(const char *key, const char *value);
 int sort_list(shash_table_t *ht, unsigned long int hash);
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
