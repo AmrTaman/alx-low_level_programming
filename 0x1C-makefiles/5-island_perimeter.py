59426e26eee9053f5b18abe3314f5cfa2e832cb7
AmrTaman <amrtaman4325@gmail.com>
2024-03-02 00:07:20 +0000

    d

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module to calculate island perimeter
+interview task
+AmrTAMAN
+"""
+
+
+def island_perimeter(grid):
+    """
+    working function for calc of perimeter
+    Well done
+    """
+    perimeter = 0
+    rows = len(grid)
+    colm = len(grid[1])
+    for x in range(rows):
+        for y in range(colm):
+            if (grid[x][y] == 1 and x + 1 <= rows - 1 and
+                    grid[x + 1][y] == 0):
+                perimeter += 1
+            elif (grid[x][y] == 1 and x - 1 >= 0 and grid[x - 1][y] == 0):
+                perimeter += 1
+            elif (grid[x][y] == 1 and y + 1 <= colm - 1 and
+                    grid[x][y + 1] == 0):
+                perimeter += 1
+            elif (grid[x][y] == 1 and y - 1 >= 0 and
+                    grid[x][y - 1] == 0):
+                perimeter += 1
+    return (perimeter)
