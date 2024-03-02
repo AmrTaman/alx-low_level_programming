599984b36d549e03c6870d39c4307320937b0289
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-03-02 02:15:02 +0200

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,15 +16,15 @@ def island_perimeter(grid):
     colm = len(grid[1])
     for x in range(rows):
         for y in range(colm):
-            if (grid[x][y] == 1 and x + 1 <= rows - 1 and
-                    grid[x + 1][y] == 0):
+            if (grid[x][y] == 0 and x + 1 <= rows - 1 and
+                    grid[x + 1][y] == 1):
                 perimeter += 1
-            elif (grid[x][y] == 1 and x - 1 >= 0 and grid[x - 1][y] == 0):
+            elif (grid[x][y] == 0 and x - 1 >= 0 and grid[x - 1][y] == 1):
                 perimeter += 1
-            elif (grid[x][y] == 1 and y + 1 <= colm - 1 and
-                    grid[x][y + 1] == 0):
+            elif (grid[x][y] == 0 and y + 1 <= colm - 1 and
+                    grid[x][y + 1] == 1):
                 perimeter += 1
-            elif (grid[x][y] == 1 and y - 1 >= 0 and
-                    grid[x][y - 1] == 0):
+            elif (grid[x][y] == 0 and y - 1 >= 0 and
+                    grid[x][y - 1] == 1):
                 perimeter += 1
     return (perimeter)
